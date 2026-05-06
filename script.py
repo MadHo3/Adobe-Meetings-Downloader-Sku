ed783fa519990b7276de217cbd508f768a552b9f
MadHo3 <themadho3@gmail.com>
2026-05-06 09:44:31 +0330

    fix: add missing feature parameter to login requests for Adobe Connect authentication

--- script.py
+++ script.py
@@ -9,18 +9,25 @@ def creat_session(username, password, lms) -> requests.session:
     s = requests.session()
     url = f"https://lms{lms}.sku.ac.ir/system/login?domain=lms{lms}.sku.ac.ir&next=/admin?domain=lms{lms}.sku.ac.ir&set-lang=en"
 
-    data = {"login": username, "password": password}
+    data = {
+        "login": username,
+        "password": password,
+        "feature=fGhJT-PGEiFYwFBGg3k43A__": "Login",
+    }
     try:
         r = s.post(url, data, timeout=20)
         r.raise_for_status()
         if "Invalid credentials." in r.text:
             print("Login Failed")
+            isLogin = False
         else:
+            isLogin = True
             print("Login Successful")
     except HTTPError as e:
         print(f"Request failed => {e}")
 
-    return s
+    print(r.text)
+    return s, isLogin
 
 
 def download_class_files(s, url):
@@ -56,9 +63,13 @@ def main():
     if "lms2" in download_url:
         lms_server = 2
 
-    user_session = creat_session(username, national_id, lms_server)
+    user_session, isLogin = creat_session(username, national_id, lms_server)
 
-    download_class_files(user_session, download_url)
+    if isLogin:
+        print("Downloading files !!! ")
+        download_class_files(user_session, download_url)
+    else:
+        print("Please login to your account :( ")
 
 
 if __name__ == "__main__":
